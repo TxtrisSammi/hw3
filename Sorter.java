f141e63be8dd5923a140b6ad47f841f39a21aabd
Samantha Morales <120693377+TxtrisSammi@users.noreply.github.com>
2025-09-10 16:18:04 -0500

    Added counting sort

--- Sorter.java
+++ Sorter.java
@@ -2,27 +2,33 @@ package hw3;
 
 import java.util.Arrays;
 
-public class Sorter implements GodricsHat{
+public class Sorter implements GodricsHat {
     public void insertion(int[] array) {
         Arrays.sort(array);
     }
 
-    public void merge(int[] array) {
+    public void merge(int[] array) { // use recursion only
         Arrays.sort(array);
     }
 
-    public void quick(int[] array, int p, int r) {
+    public void quick(int[] array, int p, int r) { // use recursion only
         Arrays.sort(array);
-    } 
+    }
 
     public void quickLoopy(int[] array) {
         Arrays.sort(array);
     }
 
     public void counting(int[] array) {
-        Arrays.sort(array);
-    } 
-    
-    
-    
+        int i = 0, j = 0, k = 0, max = Integer.MIN_VALUE;
+        for (i = 0; i < array.length; i++)
+            max = array[i] > max ? array[i] : max;
+        int[] counts = new int[max + 1];
+        for (i = 0; i < array.length; i++)
+            counts[array[i]]++;
+        for (i = 0; i < counts.length; i++)
+            for (j = 0; j < counts[i]; j++)
+                array[k++] = i;
+    }
+
 }
