3558ae794a2d26f5870d39f14e8946a93ad82b26
Samantha Morales <120693377+TxtrisSammi@users.noreply.github.com>
2025-09-10 15:56:03 -0500

    pushed initial 3 files

---
+++ GodricsHat.java
@@ -0,0 +1,12 @@
+package hw3;
+public interface GodricsHat {
+    void insertion(int[] array);
+
+    void merge(int[] array); // use recursion only
+
+    void quick(int[] array, int p, int r); // use recursion only
+
+    void quickLoopy(int[] array);
+
+    void counting(int[] array);
+}
---
+++ Sorter.java
@@ -0,0 +1,28 @@
+package hw3;
+
+import java.util.Arrays;
+
+public class Sorter implements GodricsHat{
+    public void insertion(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public void merge(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public void quick(int[] array, int p, int r) {
+        Arrays.sort(array);
+    } 
+
+    public void quickLoopy(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public void counting(int[] array) {
+        Arrays.sort(array);
+    } 
+    
+    
+    
+}
